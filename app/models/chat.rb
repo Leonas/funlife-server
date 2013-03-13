d0ec5b18d30744e7de189ed5d5bf8db05c66eab5
Leonas Liudzius <leonasl@gmail.com>
2013-03-13 05:13:14 -0700

    .gitignore is now working

--- app/models/chat.rb
+++
@@ -1,6 +0,0 @@
-class Chat < ActiveRecord::Base
-  # attr_accessible :title, :body
-
-  has_and_belongs_to_many :users
-  has_many :chat_messages
-end
--- app/models/chat_message.rb
+++
@@ -1,7 +0,0 @@
-class ChatMessage < ActiveRecord::Base
-
-  attr_accessible :message
-
-  belongs_to :user
-  belongs_to :chat
-end

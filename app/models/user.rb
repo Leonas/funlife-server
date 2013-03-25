13eff989715362dcf841ba7a3c4b1b6eb959ada5
robertomiranda <rjmaltamar@gmail.com>
2013-03-25 10:08:14 -0500

    remove duplicated methods on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,22 +8,6 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email
   before_save :ensure_authentication_token!
 
-  def self.find_by_email(email)
-    if User.where(email: email).exists?
-      User.where(email: email).first
-    else
-      nil
-    end
-  end
-
-  def self.find_by_token(token)
-    if User.where(token: token).exists?
-      User.where(token: token).first
-    else
-      nil
-    end
-  end
-
   def ensure_authentication_token!
     self.token ||= SecureRandom.urlsafe_base64
   end

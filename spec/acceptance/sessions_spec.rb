68ac912d4a5a34e4d114146fd4a09a64f5434b25
Leonas Liudzius <leonasl@gmail.com>
2013-09-27 14:19:07 -0700

    added a few failing specs

---
+++ spec/acceptance/sessions_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+require 'rspec_api_documentation/dsl'
+
+resource "Sessions" do
+
+  header "Accept", "application/json"
+  header "Content-Type", "application/json"
+
+  let(:email)    { "email@email.com" }
+  let(:password) { "example" }
+  let(:raw_post) { params.to_json }
+
+
+  post "/sessions" do
+    parameter :email,    "User email",    required: true, scope: :user
+    parameter :password, "User password", required: true, scope: :user
+
+    example_request "Create a session (login)" do
+      explanation "A user logs in"
+      response_body.should be_json_eql({
+
+                                           user: {
+                                               email: email,
+                                               followers_count: 0,
+                                               following_count: 0,
+                                               name: " ",
+                                               token: JSON.parse(response_body)['user']['token']
+                                           }
+
+                                       }.to_json)
+      status.should == 201
+    end
+  end
+
+  delete "/sessions" do
+    example_request "Destroy a session (logout)" do
+      explanation "A user logs out"
+      status.should == 204
+    end
+  end
+
+end
--- spec/acceptance/users_spec.rb
+++ spec/acceptance/users_spec.rb
@@ -1,5 +1,3 @@
-#this isnt correct but a trial of how it could be
-
 require 'spec_helper'
 require 'rspec_api_documentation/dsl'
 
@@ -8,35 +6,114 @@ resource "Users" do
   header "Accept", "application/json"
   header "Content-Type", "application/json"
 
-  let(:user) { User.create(:email => "chub934@gmau.com", :password => "dubdubdub") }
+  let(:email)    { "email@email.com" }
+  let(:password) { "example" }
+  let(:raw_post) { params.to_json }
+
 
 
   post "/users" do
     parameter :email,    "User email",    required: true, scope: :user
     parameter :password, "User password", required: true, scope: :user
 
-    let(:email)    { "email@email.com" }
-    let(:password) { "example" }
-
-
-    let(:raw_post) { params.to_json }
-
-
     example_request "Creating an account" do
-      explanation "Email and password are sent. Email and Token are received"
+      explanation "User signs up with unique email and password. No header auth required."
       response_body.should be_json_eql({
 
-                                           user: {
-                                               email: email,
-                                               followers_count: 0,
-                                               following_count: 0,
-                                               name: " ",
-                                               token: JSON.parse(response_body)['user']['token']
-                                           }
-                                       }.to_json)
+        user: {
+            email: email,
+            followers_count: 0,
+            following_count: 0,
+            name: " ",
+            token: JSON.parse(response_body)['user']['token']
+        }
+
+      }.to_json)
       status.should == 201
+    end
+  end
+
+  put "/users" do
+    parameter :first_name,  "First name", required: true, scope: :user
+    parameter :last_name,   "Last name",  required: true, scope: :user
+    parameter :gender,      "Gender",     scope: :user
+    parameter :birthday,    "Birthday",   scope: :user
+
+    example_request "Complete the signup process / update a user" do
+      explanation "A user needs to have their first and last name on file before they can view any API endpoints"
+
+      status.should == 204 #no content?
 
     end
   end
+
+
+  get "/users" do
+    #need to make factories create 2 fake users
+    example_request "List nearby users" do
+      explanation "A list of users is returned based on proximity to their gps coordinates"
+      response_body.should be_json_eql({
+
+        users: [
+          {
+            id: user1.id,
+            full_name: user1.full_name,
+          },
+          {
+            id: user2.id,
+            full_name: user2.full_name,
+          }
+        ]
+
+    })
+    end
+  end
+
+
+  get "/users/:id" do
+    parameter :id, "id", required: true
+    let(:id) { user.id }
+
+    example_request "Fetch a user's profile" do
+      explanation "user profile"
+      response_body.should be_json_eql({
+      #                                     "user": {
+      #    "id": "1",
+      #    "name"           : "First Last",
+      #    "photo"          : "photo_url",
+      #    "follower_count" : "23",
+      #    "following_count": "34",
+      #    "status"         : "single or blank",
+      #    "verified"       : "true",      //ribbon
+      #"trusted"        : "false",     //ribbon
+      #"trust"          : "false",     //do I trust this person?
+      #"follow"         : "true",      //do I follow this person?
+      #"feedback"       : "75",
+      #    "invite_me" {
+      #    "standard_activities": [
+      #    "img_url",
+      #    "img_url",
+      #    "img_url"
+      #],
+      #    "custom_activities": "shopping, eating donuts, pool parties"
+      #},
+      #    "activities_completed": [
+      #    { "img": "url", "times": "6" },
+      #    { "img": "url", "times": "4" },
+      #    { "img": "url", "times": "1" },
+      #],
+      #    "questions": [
+      #    { "title": "introduction", "answer": "hello" },
+      #    { "title": "enjoyment", "answer": "biking" }
+      #]
+      #
+      #}
+                                       })
+    end
+
+
+
+  end
+
 
 end
